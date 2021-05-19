23ca1df855eeebf62b52f542d83a5fafa8777618
amalahema <70412738+amalahema@users.noreply.github.com>
2021-05-19 15:54:50 -0500

    Start a Server Using node module

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
-var rect = require('./rectangle');
-
-
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
-
-    rect(l,b, (err, rectangle) => {
-        if (err)
+//Start up a simple web server  through http node
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const hostname = 'localhost';
+const port = 3000;
+const server = http.createServer((req,res) => {
+    console.log("Request for " + req.url +'by method' + req.method);
+    if(req.method == 'GET')
+    {
+        var fileUrl;
+        if(req.url =='/')
+           fileUrl = '/index.html';
+        else 
+           fileUrl = req.url;
+        var filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html')
         {
-        console.log("error: " ,err.message);
+            fs.exists(filePath,(exists) => {
+                if(!exists)
+                {
+                    res.statusCode = 404;
+                    res.setHeader('ContentType','text/html');
+                    res.end('<html><body><h1>Error 404:' +fileUrl+ 'not found</h1></body></html>');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+              });
+            
         }
         else
         {
-        console.log("The area of rectangle of dimension l=" +l+ "and b=" +b+ "is" + rectangle.area());
-        console.log("The perimeter of rectangle of dimension l=" +l+ "and b=" +b+ "is" + rectangle.area());
+            res.statusCode = 404;
+            res.setHeader('ContentType','text/html');
+            res.end('<html><body><h1>Error 404:' +fileUrl+ 'not an HTML file</h1></body></html>');
+            return;
         }
-    });
-    console.log("This statement is after the callto rect()");
-    //rect is a node module with 2 parameter and 1 call back  function(2 parameter)
-}
-
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+        
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('ContentType','text/html');
+        res.end('<html><body><h1>Error 404:' +req.method+ 'not supported</h1></body></html>');
+        return;
+    }
+    
+})
+server.listen(port, hostname, () => {
+    console.log(`Server Running at http://${hostname}:${port}`)
+    //if you include something inside dollar raises, that will be replaced by its value in this string here.
+});
